c7dbd4f47355c235359c72bc381ceeb0c7296877
John Johnson-Rodgers <john@johnthe.dev>
2019-10-08 10:47:20 -0600

    create user-story persona use-case-interaction-overflow conceptual-model and stylecss

---
+++ epic/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF8">
+	<title>Data Design-Wikipedia</title>
+	<link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+	<div class="wrap">
+		<h1>Data Design-Wikipedia</h1>
+		<ul>
+			<li><a href="persona.html" class="link">Persona</a></li>
+			<li><a href="user-story.html" class="link">User Story</a></li>
+			<li><a href="use-case-interaction-overflow.html" class="link">Use Case/Interaction Flow</a></li>
+			<li><a href="conceptual-model.html" class="link">Conceptual Model</a></li>
+		</ul>
+	</div>
+</body>
+</html>
+
